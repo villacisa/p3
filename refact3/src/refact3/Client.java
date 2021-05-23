2149d59effe0900d65dc8011b4fa2dcdb3b28103
macbookair <villacisajd@gmail.com>
2021-05-23 21:34:56 +0200

    Tercera part on mouem el metode quantitatPerLloguer() a Lloguer.

---
+++ refact3/src/refact3/Client.java
@@ -0,0 +1,76 @@
+package refact3;
+
+import java.util.ArrayList;
+
+
+public class Client {
+	private String nif;
+    private String nom;
+    private String telefon;
+    private ArrayList<Lloguer> lloguers;
+    
+    public Client(String nif, String nom, String telefon) {
+        this.nif = nif;
+        this.nom = nom;
+        this.telefon = telefon;
+        this.lloguers = new ArrayList<Lloguer>();
+    }
+
+    public String getNif()     { return nif;     }
+    public String getNom()     { return nom;     }
+    public String getTelefon() { return telefon; }
+
+    public void setNif(String nif) { this.nif = nif; }
+    public void setNom(String nom) { this.nom = nom; }
+    public void setTelefon(String telefon) {
+        this.telefon = telefon;
+    }
+    public void afegeix(Lloguer lloguer) {
+        if (! lloguers.contains(lloguer) ) {
+            lloguers.add(lloguer);
+        }
+    }
+    public void elimina(Lloguer lloguer) {
+        if (lloguers.contains(lloguer) ) {
+            lloguers.remove(lloguer);
+        }
+    }
+    public boolean conte(Lloguer lloguer) {
+        return lloguers.contains(lloguer);
+    }
+
+    public String informe() {
+    	double total = 0;
+        int bonificacions = 0;
+        String resultat = "Informe de lloguers del client " +
+            getNom() +
+            " (" + getNif() + ")\n";
+        for (Lloguer lloguer: lloguers) {
+            double quantitat = lloguer.quantitat();
+
+            // afegeix lloguers freqüents
+            bonificacions ++;
+
+            // afegeix bonificació per dos dies de lloguer de Luxe
+            if (lloguer.getVehicle().getCategoria() == Vehicle.LUXE &&
+                    lloguer.getDies()>1 ) {
+                bonificacions ++;
+            }
+
+            // composa els resultats d'aquest lloguer
+            resultat += "\t" +
+                lloguer.getVehicle().getMarca() +
+                " " +
+                lloguer.getVehicle().getModel() + ": " +
+                (quantitat * 30) + "€" + "\n";
+            total += quantitat * 30;
+        }
+
+        // afegeix informació final
+        resultat += "Import a pagar: " + total + "€\n" +
+            "Punts guanyats: " + bonificacions + "\n";
+        return resultat;
+        
+    }
+}
+    
---
+++ refact3/src/refact3/Lloguer.java
@@ -0,0 +1,62 @@
+package refact3;
+
+import java.util.Date;
+import java.util.ArrayList;
+
+public class Lloguer {
+	private Date data;
+	private int dies;
+	private Vehicle vehicle;
+	
+	public Lloguer(Date data, int dies, Vehicle vehicle) {
+		this.data = data;
+		this.dies = dies;
+		this.vehicle = vehicle;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	public int getDies() {
+		return dies;
+	}
+
+	public void setDies(int dies) {
+		this.dies = dies;
+	}
+
+	public Vehicle getVehicle() {
+		return vehicle;
+	}
+
+	public void setVehicles(Vehicle vehicle) {
+		this.vehicle = vehicle;
+	}
+	
+	public double quantitat() {
+		double retorna_quantitat = 0;
+		switch (getVehicle().getCategoria()) {
+        case Vehicle.BASIC:
+            retorna_quantitat += 3;
+            if (getDies() > 3) {
+                retorna_quantitat += (getDies() - 3) * 1.5;
+            }
+            break;
+        case Vehicle.GENERAL:
+            retorna_quantitat += 4;
+            if (getDies() > 2) {
+                retorna_quantitat += (getDies() - 2) * 2.5;
+            }
+            break;
+        case Vehicle.LUXE:
+            retorna_quantitat += getDies() * 6;
+            break;
+    }
+		return retorna_quantitat;
+	}
+}
